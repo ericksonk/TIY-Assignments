c1f7c4d20facf578949e825b5b999d4493a7aa60
Kristine Erickson <ericksonk09@gmail.com>
2015-09-03 14:14:56 -0400

    2 tests passing

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,7 +5,14 @@ test('fromEnglish: "zero" through "nine"', function(){
   expect(fromEnglish).to.exist;
   expect(fromEnglish("zero")).to.equal(0);
   expect(fromEnglish("one")).to.equal(1);
-  // more tests here, please...
+  expect(fromEnglish("two")).to.equal(2);
+  expect(fromEnglish("three")).to.equal(3);
+  expect(fromEnglish("four")).to.equal(4);
+  expect(fromEnglish("five")).to.equal(5);
+  expect(fromEnglish("six")).to.equal(6);
+  expect(fromEnglish("seven")).to.equal(7);
+  expect(fromEnglish("eight")).to.equal(8);
+  expect(fromEnglish("nine")).to.equal(9);
 }); // END test(fromEnglish)
 
 /**
@@ -13,14 +20,43 @@ test('fromEnglish: "zero" through "nine"', function(){
  * @return {Number} value corresponding to `word`
  */
 function fromEnglish(word){
-  // fill this in after writing tests...
+  if(word === "zero"){
+    return 0;
+  }
+  if(word === "one"){
+    return 1;
+  }
+  if(word === "two"){
+    return 2;
+  }
+  if(word === "three"){
+    return 3;
+  }
+  if(word === "four"){
+    return 4;
+  }
+  if(word === "five"){
+    return 5;
+  }
+  if(word === "six"){
+    return 6;
+  }
+  if (word === "seven"){
+    return 7;
+  }
+  if (word === "eight"){
+    return 8;
+  }
+  if (word === "nine"){
+    return 9;
+  }
 } // END fromEnglish
 
 test('plus: ("zero", "zero") through ("zero", "nine")', function(){
   expect(plus).to.exist;
   expect(plus("zero", "zero")).to.equal(0);
   expect(plus("zero", "one")).to.equal(1); // change just one thing!
-  expect(plus("zero", "two")).to.equal(FILL_ME_IN); // ha!
+  expect(plus("zero", "two")).to.equal(2); // ha!
   // write more tests like those...
 }); // END test(plus)
 
@@ -30,7 +66,36 @@ test('plus: ("zero", "zero") through ("zero", "nine")', function(){
  * @return {Number} the value of `A` added to `B`
  */
 function plus(A, B){
-  // write those tests first, please...
+  if(A === "zero", B === "zero"){
+    return 0;
+  }
+  if(A === "zero", B === "one"){
+    return 1;
+  }
+  if(A === "zero", B === "two"){
+    return 2;
+  }
+  if(A === "zero", B === "three"){
+    return 3;
+  }
+  if(A === "zero", B === "four"){
+    return 4;
+  }
+  if(A === "zero", B === "five"){
+    return 5;
+  }
+  if(A === "zero", B === "six"){
+    return 6;
+  }
+  if(A === "zero", B === "seven"){
+    return 7;
+  }
+  if(A === "zero", B === "eight"){
+    return 8;
+  }
+  if(A === "zero", B === "nine"){
+    return 9;
+  }
 } // END plus
 
 test('minus: ("zero", "zero") through ("zero", "nine")', function(){
