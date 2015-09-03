aba3127ded55d35488d7fcd6add521d8e5c3dce2
Kristine Erickson <ericksonk09@gmail.com>
2015-09-02 20:35:16 -0400

    Attempted...
    Attempted to fill-in but couldn't get mocha test to run in my terminal :(

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,1 +1,55 @@
 
+var test = require('mocha').it,
+  expect = require('chai').expect;
+
+test('fromEnglish: "zero" through "nine"', function(){
+  expect(fromEnglish).to.exist;
+  expect(fromEnglish("zero")).to.equal(0);
+  expect(fromEnglish("one")).to.equal(1);
+  expect(fromEnglish("two")).to.equal(2);
+  expect(fromEnglish("three")).to.equal(3);
+  expect(fromEnglish("four")).to.equal(4);
+  expect(fromEnglish("five")).to.equal(5);
+  expect(fromEnglish("six")).to.equal(6);
+  expect(fromEnglish("seven")).to.equal(7);
+  expect(fromEnglish("eight")).to.equal(8);
+  expect(fromEnglish("nine")).to.equal(9);
+  // more tests here, please...
+}); // END test(fromEnglish)
+
+/**
+ * @param {String} word representing an English number
+ * @return {Number} value corresponding to `word`
+ */
+ function fromEnglish("Number"){
+   return ;
+ }
+ // END fromEnglish
+
+test('plus: ("zero", "zero") through ("zero", "nine")', function(){
+  expect(plus).to.exist;
+  expect(plus("zero", "zero")).to.equal(0);
+  expect(plus("zero", "one")).to.equal(1); // change just one thing!
+  expect(plus("zero", "two")).to.equal(2); // ha!
+  expect(plus("zero", "three")).to.equal(3);
+  expect(plus("zero", "four")).to.equal(4);
+  expect(plus("zero", "five")).to.equal(5);
+}); // END test(plus)
+
+/**
+ * @param {String} A representing an English number word
+ * @param {String} B representing an English number word
+ * @return {Number} the value of `A` added to `B`
+ */
+function plus(A, B){
+  // write those tests first, please...
+} // END plus
+
+test('minus: ("zero", "zero") through ("zero", "nine")', function(){
+  expect(minus).to.exist;
+  expect(minus("zero", "zero")).to.equal(0);
+  expect(minus("zero", "one")).to.equal(-1);
+  // etc, etc, etc
+}); // END test(minus)
+
+// What about multiplication? Division? Exponents?
