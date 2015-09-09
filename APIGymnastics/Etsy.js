172aec95fe845def97ff6ace4223261227e1c1ac
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 00:03:54 -0400

    1 test passing; working on passing average and

--- APIGymnastics/Etsy.js
+++ APIGymnastics/Etsy.js
@@ -18,15 +18,19 @@ function amount(items) {
   return items.length;
 }
 
-test('finding the average price', function(){ //items.json = 1 giant array. need to call property (i.e. price) to this file
+test('finding the average price', function(){ //items.json = array. need to call property (price) to this file
   var yourAnswer = "start with `items`; use `Array` methods";
 
   expect(yourAnswer).to.be.closeTo(23.63, 0.01);
 });
 
-function avPrice() {
-  items["price"] = 
-
+function averagePrice() {
+  items["price"] = "avPrice";//calls prices property in .json // trying to figure out how to return array w/ all the prices
+    for (i = 0, sum = 0; i < prices.length; sum += prices[i++]){
+    //starts counting @ index value 0, sum starts @ 0;
+    //will run length of `prices`; `+=` adds each item in `prices` to `sum`
+      return sum / items.length;
+    }
 }
 
 test('finding that perfect $15 item', function(){
@@ -34,14 +38,24 @@ test('finding that perfect $15 item', function(){
    * @param {Array} items to search through
    * @return {Array} of `items` with `price` between `min` and `max` USD
    */
-  function pricedBetween(items, min, max){
-    // Just a suggestion, really...
+  function pricedBetween(items){
+    var min = 14;
+    var max = 18;
+    for (i = 0; i < items.length; i++){//need to create a `for` loop that finds all prices less than 18 and more than 14
+      if (i > min && i < max){
+        return items.map(i);// .map()????
+      }
+    }
+    // if (items >= min && items <= max){
+    //   return items;
+    }
   }
+// var filtered = items.price.filter(pricedBetween);//items[10] bc 'price' is the 10th index in items.json array
 
-  expect( pricedBetween(items, 14, 18) ).to.deep.equal([
+  //expect( pricedBetween(items, 14, 18) ).to.deep.equal([
     // Can you find their names _without_ code first?
-  ]);
-});
+   ]);
+ });
 
 test('pond hopping', function(){
   expect( /* your answer */ ).to.deep.equal({
