e92e9b3bab35397366eeb61c7c600e4193df5f12
Kristine Erickson <ericksonk09@gmail.com>
2015-09-10 00:00:03 -0400

    basic outline on how to approach fixing w/ js

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -13,15 +13,13 @@ file w/ js:
 
 How to fix:
 * write function that lets you `switch` view modes from grid to list
-*
+* create `for` loops for gird and list
+* target class `cbp-vm-switcher` with getElementByClassName
 
-switch (expression) { //whoa Atom tryed to help me when i typed `switch`... might be helpful????
+switch (expression) { //whoa Atom spit this out when i typed `switch`... might be helpful????
   case expression:
 
     break;
   default:
 
-} grid and list buttons
-*
-*
-*
+}
