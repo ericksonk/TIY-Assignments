449dda9824876ab93b2e1ea213992442e8634aa2
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 23:06:02 -0400

    create array, add function calling array, getElementsByTagName, onclick event handler, ontoggle, ???

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -27,10 +27,14 @@ Also inside of the div element is another unordered list with the class of "cbp-
 Each item in the sub-list has a header with the class "cbp-nttrigger" and div element with the class "cbp-ntcontent"
 
 In order to use JavaScript with our HTML file, link to our .js file within a `<script>` tag on the .html page with the source set to the file path
-We need to write a function that calls the id "cpb-ntaccordion" using the `Document` prototype document.getElementById()
-Need to assign <h3> element a `toggleItem()` onclick `event handler`.
+Need to create an array to hold the accordion items???
+Create function to initiate JavaScript???
+  * use getElementsByTagName() to call array
+  * create for loop with if statements that trigger different items either onload or ontoggle
+Need to write a `toggleItem()` onclick function that is assigned to the <h3> element.
   // toggleItem() hides or shows all the items in the list when it is clicked on (onclick)
-  
+    // aka will expand and collapse accordion
+Need to call the id "cpb-ntaccordion" using the `Document` prototype document.getElementById()
 
 
 
