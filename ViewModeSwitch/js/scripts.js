d5be2d71a0a25074591af4ca01e0550dbd6ac88f
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 23:50:12 -0400

    need to figure out how to write fuctions to switch view modes

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -9,3 +9,19 @@ file w/ js:
 * content is interactive and you can toggle over gird or list buttons to change the view
 * add-to-cart buttons for each item
 * images for each item
+
+
+How to fix:
+* write function that lets you `switch` view modes from grid to list
+*
+
+switch (expression) { //whoa Atom tryed to help me when i typed `switch`... might be helpful????
+  case expression:
+
+    break;
+  default:
+
+} grid and list buttons
+*
+*
+*
