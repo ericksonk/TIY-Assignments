122ae54ce40f13d212626d36711c52a87ca11e26
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 23:41:26 -0400

    differences between js and nonjs pages

---
+++ ViewModeSwitch/js/scripts.js
@@ -0,0 +1,11 @@
+file w/o js:
+* can not choose between grid or list view
+* bulleted list
+* no item images
+* no add to cart button
+
+
+file w/ js:
+* content is interactive and you can toggle over gird or list buttons to change the view
+* add-to-cart buttons for each item
+* images for each item
