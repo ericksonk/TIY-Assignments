d70bc42e41a90fe6b18cc9e32b4a7dffc1634328
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 21:02:42 -0400

    here we go...

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,6 +1,9 @@
 /*
 What does it do?
   What interactive elements exist?
+    * headings- when you click on the headings they open accordion style and then
+      there are accordions with in accordions
+    * navagation buttons- `::before` & `::after` js???
   How do you interact with them?
   What visual effects are produced by interacting?
 
@@ -8,4 +11,16 @@ How does it do it?
  What existing HTML elements are changed?
  What new HTML elements are created?
  What CSS styles are used to produce the effect?
+  * `cursor: pointer` controls animated arrow?
+  *
   **/
+
+The HTML language is assigned to english and the class should be assigned to Javascript.
+
+When accordion is opened list has a class of 'cbp-ntopen'
+
+The list with in the header tag 
+
+
+// elements and assigning elements- look at DOM terms
+// onclickeventhandler - changes color of font when you hover
