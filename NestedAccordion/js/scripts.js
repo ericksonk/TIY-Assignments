46b477d899bf4a2282402cde17d7067ce70374bf
Kristine Erickson <ericksonk09@gmail.com>
2015-09-09 21:43:35 -0400

    explained html set up, now working on how to incorporate js

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -16,10 +16,18 @@ How does it do it?
   **/
 
 The HTML language is assigned to english and the class should be assigned to Javascript.
+The bulk of the document is an unordered list with `<h3>` tags for each heading.
+The unordered list has an id of "cpb-ntaccordion" and a class of "cbp-ntaccordion"
+The `h3` element has a class set to "cpb-nttrigger"
+Each heading accordions open to a description and sub-accordions.
+When accordion is opened, list has a class of 'cbp-ntopen'
+Inside of a <div> element, a description is written inside a paragraph tag.
+The div element has a class set to "cbp-ntcontent"
+Also inside of the div element is another unordered list with the class of "cbp-ntsubaccordion"
+Each item in the sub-list has a header with the class "cbp-nttrigger" and div element with the class "cbp-ntcontent"
 
-When accordion is opened list has a class of 'cbp-ntopen'
 
-The list with in the header tag 
+The list with in the header tag
 
 
 // elements and assigning elements- look at DOM terms
