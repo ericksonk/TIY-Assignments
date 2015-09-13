55c11b7ed00093787d1d08b9a5666d85c8d95caf
Kristine Erickson <ericksonk09@gmail.com>
2015-09-13 15:37:33 -0400

    MCF- first name logs to console

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -4,3 +4,26 @@
  * wrtie function for each element? that will log the form?
  * add event listener?
  **/
+
+// var form = document.getElementsByClassName('.cbp-mc-form');
+var fName = document.getElementById('first_name');
+// var lName =
+// var email =
+// var country =
+// var bio =
+// var phone =
+// var affiliate =
+// var job =
+// var cat =
+// var gadget =
+// var talent =
+// var drink =
+// var superPower =
+// var weapon =
+// var comment =
+
+function firstName (){
+  console.log(fName.value);
+}
+
+fName.addEventListener('input', firstName);
