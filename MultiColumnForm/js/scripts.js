657cc3f4fff9ae40e81d1770b82d2cc1e91cf23e
Kristine Erickson <ericksonk09@gmail.com>
2015-09-13 14:15:34 -0400

    MCF- pseudo goals

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -1,1 +1,6 @@
- 
+ /* *** GOAL: TURN USER'S INPUT INTO OUTPUT ***
+ * set variable for entrie form
+ * set variable for each element that will return data inside of form
+ * wrtie function for each element? that will log the form?
+ * add event listener?
+ **/
