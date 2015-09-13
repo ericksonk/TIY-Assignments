d4ed4954f07df2dd85ff2ca400a1643656ca79a8
Kristine Erickson <ericksonk09@gmail.com>
2015-09-13 18:07:56 -0400

    trying to add click listener to submit button

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -7,16 +7,43 @@
 
 // var form = document.getElementsByClassName('.cbp-mc-form');
 // console.log(form);
+
 var inputIds = document.querySelectorAll('input'); // create array of all id's in input tag
-for (i > 0; i < inputIds.length; i++){
+console.log(inputIds);
+var sendData = document.querySelector(".cbp-mc-submit"); // targets `send your data button`
+
+for (var i = 0; i < inputIds.length; i++){
   inputsId[i].addEventListener('input', function(){
     for (i > 0; inputIds.length; i++){
-      console.log(inputIds[i].value);
+      output = inputsId[i].element.value;
+      console.log(output);
     }
   });
 }
 
+//
+// function data (){
+//   for (var i = 0; i < inputIds.length; i++){
+//     inputIds[i].addEventListener('input', data);
+//     output = inputIds[i].value;
+//     console.log(output);
+//   }
+// }
 
+
+
+//POSSIBLE FOR LOOP STRUCTURE-
+// for (var i = 0; i < inputIds.length; i++){
+//   inputsId[i].addEventListener('input', function(){
+//     for (i > 0; inputIds.length; i++){
+//       output = inputsId[i].element.value;
+//       console.log(output);
+//     }
+//   });
+// }
+
+
+//MAYBE USE THIS STUFF?
 // console.log(data.value);
 // fName.addEventListener('input', data);
 //
