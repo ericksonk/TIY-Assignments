1be2fea9ed75c85ef508db94b3a69831ff15c20a
Kristine Erickson <ericksonk09@gmail.com>
2015-09-27 16:53:17 -0400

    ViewMode- tyring to figure out which classes i want to add and remove

---
+++ ViewModeSwitch/js/jq-2.1.3.js
@@ -0,0 +1,8 @@
+$('.cbp-vm-icon').on('click', function() {
+  if ($(this).hasClass('.cbp-vm-grid .cbp-vm-selected')) {
+    $('#cbp-vm').removeClass().addClass();
+  }
+  if ($(this).hasClass('.cbp-vm-grid .cbp-vm-selected')) {
+    $('#cbp-vm').removeClass().addClass();
+  }
+});
