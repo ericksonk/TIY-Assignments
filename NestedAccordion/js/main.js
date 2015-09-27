8ae599626e6ea9c1ba216a8806708b05f2f6df69
Kristine Erickson <ericksonk09@gmail.com>
2015-09-27 12:05:58 -0400

    Nested- subaccordions opening and closing

--- NestedAccordion/js/main.js
+++ NestedAccordion/js/main.js
@@ -1,14 +1,24 @@
+// OPENS AND CLOSES ACCORDIONS
 $('section.content').hide(); // hides <p> elements & sub-accordions
 
-$('h2').on('click', function(){
-  if ($(this).next().is(':hidden')) {
-      $(this).next().show();
+$('h2').on('click', function(){ // adds event listener to all <h2> elements
+  if ($(this).next().is(':hidden')) { // if next tag after current (this) element (h2) is hidden...
+      $(this).next().show(); // ..then show it
   } else {
       $(this).next().hide();
     }
 });
 
-$('h3 ~ p').hide();
+// OPENS & CLOSES SUB ACCORDIONS
+$('h3 ~ p').hide(); // hides sub-accordion's content (<p>)
+
+$('h3').on('click', function(){ // adds event listener to all <h3> elements
+  if ($(this).next().is(':hidden')) { // if next tag after current (this) element (h3) is hidden...
+      $(this).next().show(); // ..then show it
+  } else {
+      $(this).next().hide();
+    }
+});
 
 // $('li').each(function() {
 //   $(this).show();
