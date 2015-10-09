590b6f9c6d3c60614352c43b8fbcc6f1e44e6470
Kristine Erickson <ericksonk09@gmail.com>
2015-10-09 13:30:29 -0400

    adds class'active'

--- HorizontalDropMenu/js/main.js
+++ HorizontalDropMenu/js/main.js
@@ -1,3 +1,17 @@
 $('nav.tabs a').on('click', function() { // selects nav element w/ class `tabs` and adds event listener
-  $(this).addClass('active');
+  // if($(this).hasClass('active'));
+  //   $(this).removeClass('active');
+
+  if($(this).hasClass(''));
+    $(this).addClass('active');
+
+
+
+  // $("").removeClass('active');
+  //   $(this).removeClass('active');
+  // $('a').removeClass('active');
 });
+
+// $('.active').on('click', function() {
+//   $(this).removeClass('active');
+// });
