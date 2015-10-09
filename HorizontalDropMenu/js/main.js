8e49ab947726ec931aeb3fbb5f44e8932bbf25bf
Kristine Erickson <ericksonk09@gmail.com>
2015-10-09 14:35:21 -0400

    active tab switches between tabs on click, but can click off

--- HorizontalDropMenu/js/main.js
+++ HorizontalDropMenu/js/main.js
@@ -1,8 +1,10 @@
-$('nav.tabs a').on('click', function() { // selects nav element w/ class `tabs` and adds event listener
-  if(!$(this).hasClass('active')) // if this does not have class `active`...
-    $(this).addClass('active'); // ...add class `active`
-      $('nav.tabs a.active').on('click', function() {
-        // if($(this).hasClass('active'))
-          $(this).removeClass('active');
-      });
+$('nav.tabs').each(function () {
+
+  $('a').on('click', function() { // selects nav element w/ class `tabs` and adds event listener
+    if(!$(this).hasClass('active')) // if this does not have class `active`...
+      $(this).addClass('active'); // ...add class `active`
+
+    // if($(this).hasClass('active'))
+      $(this).siblings().removeClass('active');
+  });
 });
